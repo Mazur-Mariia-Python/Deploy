ff66c3e34a9fa23c65ab703f4641c74a54d5121f
ProjectTobi <mariiamazur2927@gmail.com>
2024-01-12 21:49:48 +0200

    first commit

---
+++ API/views.py
@@ -0,0 +1,47 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.utils import json
+from django.http import JsonResponse
+
+from gift_finder.models import SelectedGift
+from .functions import process_gift_picking
+from rest_framework import status
+import json
+from pydantic import ValidationError
+from .validators import QuestionnaireAnswers
+
+
+@api_view(['POST'])
+def api_load_response_data(request):
+    try:
+        questionnaire_answers_json = request.data["_content"]
+
+        # Parse the JSON data into a Python dictionary
+        questionnaire_answers = json.loads(questionnaire_answers_json)
+
+        # Validate questionnaire_answers using Pydantic
+        try:
+            QuestionnaireAnswers(**questionnaire_answers)
+            picked_gifts = process_gift_picking(questionnaire_answers)
+            return JsonResponse(picked_gifts, status=status.HTTP_200_OK, safe=False)
+        except ValidationError as e:
+            print("Validation error:", e)
+            return JsonResponse({'Validation error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def selected_gifts_list(request):
+    if request.method == "GET":
+        # Checking if the user is authenticated
+        if not request.user.is_authenticated:
+            return Response({"error": "Access denied. User is not authenticated."},
+                            status=403)
+
+        # Receiving selected gifts for a specific user
+        selected_gifts = SelectedGift.objects.filter(user=request.user)
+        serializer = SelectedGiftSerializer(selected_gifts, many=True)
+
+        return Response(serializer.data, status=200)
---
+++ gift_finder/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from Tobi import settings
+
+
+#A model that is needed to save links to a category of a certain product and pass this link to the parser
+class Product(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Назва товару')
+    link = models.URLField(verbose_name='Посилання на товар')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товари'
+
+
+"""Model for saving selected gifts"""
+
+
+class SelectedGift(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=255)
+    price = models.CharField(max_length=30)
+    image_url = models.TextField(help_text="URL of the image for the gift.")
+    link = models.TextField(help_text="URL to purchase the gift.")
+    is_bought = models.BooleanField(default=False)
+    is_selected = models.BooleanField(default=False)
+    is_delivered = models.BooleanField(default=False)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    """Sorting selected gifts from newest to oldest"""
+    class Meta:
+        ordering = ["-created_at"]
+        verbose_name_plural = "selected_gifts"
+
+    def __str__(self):
+        return str(self.name)
