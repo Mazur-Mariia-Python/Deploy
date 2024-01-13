efbdea3a71fc6949ce2f12295e3bb0793f1a199d
ProjectTobi <mariiamazur2927@gmail.com>
2024-01-13 19:27:35 +0200

    Updated settings.py & requirements.txt

--- Tobi/settings.py
+++ Tobi/settings.py
@@ -21,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-i2i1n8c*=!y8&5laf&go$j@_hpfxtj_gx%!b$^*7s0x)$!1^_u"
+# SECRET_KEY = "django-insecure-i2i1n8c*=!y8&5laf&go$j@_hpfxtj_gx%!b$^*7s0x)$!1^_u"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-i2i1n8c*=!y8&5laf&go$j@_hpfxtj_gx%!b$^*7s0x)$!1^_u")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
